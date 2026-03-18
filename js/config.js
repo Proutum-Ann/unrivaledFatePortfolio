7ed9ff74c382ad8699aa5f153992b46152b3015b
Proutum-Ann <liangproutum@gmail.com>
2026-03-18 09:59:01 -0400

    i have no idea what's happening here, trying to get the actual google sheets to not give errors

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
-let portfolios = {};
+let unrivaldF = {};
+
+unrivaldF.sheet = {
+    id: "1DZPic5mOWhjKP18dDWYlMKEKDSL7I95NYrGF6OTHTXs",
+
+    pages: {
+        portfolio: "images",
+        developers: "developers"
+    },
+
+    options: {
+        exampleType: ["Model", "VFX", "Animation", "SFX", "Art"],
+        fileType: ["Image", "Video", "Audio"],
+        imageLink: "image"
+    }
+}
